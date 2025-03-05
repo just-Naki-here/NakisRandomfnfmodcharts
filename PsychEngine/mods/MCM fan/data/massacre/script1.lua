6c553db3f7ed4045ce98299794c3f62cf76a8570
NAKIII <graystripe.yaryan@gmail.com>
2025-03-05 15:28:46 -0600

    6y56y
    ethrethr

--- PsychEngine/mods/MCM fan/data/massacre/script1.lua
+++ PsychEngine/mods/MCM fan/data/massacre/script1.lua
@@ -1,4 +1,5 @@
 dh = 0
+i=300
 function onUpdatePost()
 
     dh = health/40
@@ -8,23 +9,22 @@ function onUpdatePost()
 end
 function opponentNoteHit()
     health = getProperty('health')
-    
-    
-        setProperty('health', health - dh);
-    
+    i = math.random(0,600)
+    setProperty('health', health - dh);
 end
-function OnUpdate()
+function onUpdate()
     songPos = getSongPosition()
 	local currentBeat = (songPos/1000)
-	noteTweenY('player1', 4, defaultPlayerStrumY3 - i*math.sin((currentBeat+8*0.1)*math.pi)-50, 3)
-	noteTweenY('player2', 5, defaultPlayerStrumY1 + i*math.sin((currentBeat+8*0.1)*math.pi)-50, 3)
-	noteTweenY('player3', 6, defaultPlayerStrumY0 - i*math.sin((currentBeat+8*0.1)*math.pi)-50, 3)
-	noteTweenY('player4', 7, defaultPlayerStrumY2 + i*math.sin((currentBeat+8*0.1)*math.pi)-50, 3)
+	i = math.random(0,600)
+	noteTweenY('player1', 0, defaultPlayerStrumY3 - i*math.sin((currentBeat+8*0.1)*math.pi)-50, 3)
+	noteTweenY('player2', 1, defaultPlayerStrumY1 + i*math.sin((currentBeat+8*0.1)*math.pi)-50, 3)
+	noteTweenY('player3', 2, defaultPlayerStrumY0 - i*math.sin((currentBeat+8*0.1)*math.pi)-50, 3)
+	noteTweenY('player4', 3, defaultPlayerStrumY2 + i*math.sin((currentBeat+8*0.1)*math.pi)-50, 3)
 	
-	noteTweenX('playerX1', 4, defaultPlayerStrumX0 - i*math.sin((currentBeat+8*0.01)*math.pi)-325, 3)
-	noteTweenX('playerX2', 5, defaultPlayerStrumX1 + i*math.sin((currentBeat+8*0.01)*math.pi)-325, 3)
-	noteTweenX('playerX3', 6, defaultPlayerStrumX2 - i*math.sin((currentBeat+8*0.01)*math.pi)-325, 3)
-	noteTweenX('playerX4', 7, defaultPlayerStrumX3 + i*math.sin((currentBeat+8*0.01)*math.pi)-325, 3)
+	noteTweenX('playerX1', 0, defaultPlayerStrumX0 - i*math.sin((currentBeat+8*0.01)*math.pi)-325, 3)
+	noteTweenX('playerX2', 1, defaultPlayerStrumX1 + i*math.sin((currentBeat+8*0.01)*math.pi)-325, 3)
+	noteTweenX('playerX3', 2, defaultPlayerStrumX2 - i*math.sin((currentBeat+8*0.01)*math.pi)-325, 3)
+	noteTweenX('playerX4', 3, defaultPlayerStrumX3 + i*math.sin((currentBeat+8*0.01)*math.pi)-325, 3)
 end
 function onCreate() --no hay animacion de muerte :((((
 setPropertyFromClass('GameOverSubstate', 'characterName', 'LFGF');
