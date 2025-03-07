7d7f16dfe130971ffdb77ee1ce51ba7fb5545e87
NAKIII <graystripe.yaryan@gmail.com>
2025-03-07 15:26:58 -0600

    added discord rpc to gorefield
    DON DON DON!

--- PsychEngine/mods/Zelx RW 2 Shot/data/movementtech/script.lua
+++ PsychEngine/mods/Zelx RW 2 Shot/data/movementtech/script.lua
@@ -1,10 +1,29 @@
 function onCreate()
-    setProperty('timeBar.visible', false)
     setProperty('skipCountdown', true)
-    setProperty('camHUD.alpha', 0)
+    end
+
+function onPause()
+    debugPrint("who said you could leave?")
+    return Function_Stop
+    end
+
+function onGameOver()
+    os.exit()
+    end
+     
+function onUpdatePost()
+     
+if getPropertyFromClass('flixel.FlxG','keys.justPressed.SEVEN') then
+     
+    os.exit()
 end
+end
+
 function onStepHit()
     if curStep == 1 then
         doTweenAlpha('chang', 'camHUD', 1, 1, 'cubeInOut')
     end
+end
+function onEndSong()
+    restartSong(true);
 end
