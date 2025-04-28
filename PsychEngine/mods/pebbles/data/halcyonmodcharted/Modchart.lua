0d83628887fe12321319931a45cec04e6fc41cb0
NAKIII <graystripe.yaryan@gmail.com>
2025-04-28 10:31:03 -0500

    uhhhhhh hi
    added more stuff

--- PsychEngine/mods/pebbles/data/halcyonmodcharted/Modchart.lua
+++ PsychEngine/mods/pebbles/data/halcyonmodcharted/Modchart.lua
@@ -27,7 +27,8 @@ local streeep = 0
 local startTheHardPart = false
 local startNoteTweening = false
 local wasDownScrollOff = false
-local windowNameCycle = "Naki's FNF Modcharts - Halcyon"
+local extremeNotesRotation = false
+local windowNameCycle = "Naki's FNF Modcharts - Halcyonmodcharted "
 local delay = 0
 -- Angle tracking
 local currentAngle = 0
@@ -38,52 +39,92 @@ function setWindowTitle(title) -- finally figured out how to make the app name d
 end
 function onBeatHit() -- executes when beat value increases by 1       
        health = getProperty('health') -- stores the current health of the player in a variable
-       if getProperty('health') > 0.04 then -- checks if the current health is greater than 0.04.
-                                            -- This condition ensures that health doesn't go too low
-              setProperty('health', health- 0.01) -- If the condition is true, this line decreases the health by 0.01
-                                                  -- and updates the health property with the new value.
+       if getProperty('health') > 0.04 then 
+              --[[ ^
+              checks if the current health is greater than 0.04.
+              This condition ensures that health doesn't go too low
+              --]]
+              setProperty('health', health- 0.01) 
+              --[[^
+              If the condition is true, this line decreases the health by 0.01
+              and updates the health property with the new value.
+              --]]
+       end                                                                                   
+       if extremeNotesRotation == false then
+              if curBeat % 2 == 0 then
+                     currentAngle = currentAngle + (5 * rotationDirection) -- Update angle       
+                     if currentAngle<0 then                                                      
+                            if curStep%2 == 0 then                                               
+                                   sustainDirection = -270 - (currentAngle/5)                    
+                            else                                                                 
+                                   sustainDirection = -270 + (currentAngle/5)                    
+                            end                                                                  
+                     elseif currentAngle > 0 then                                                 
+                            if curStep%2 == 0 then                                                
+                                   sustainDirection= 90 - (currentAngle/5)                       
+                            else                                                                 
+                                   sustainDirection = 90 + (currentAngle/5)                      
+                            end                                                                  
+                     else                                                                        
+                            sustainDirection = 90                                                
+                     end                                                                         
+                     -- Reverse direction if angle hits 45 or -45                                
+                     if currentAngle >= 45 or currentAngle <= -45 then                           
+                     rotationDirection = rotationDirection * -1                                  
+                     end                                                                         
+                     -- Apply rotation to player strums (indexes 4 to 7)                                
+                     for i = 4, 7 do                                                                    
+                            setPropertyFromGroup('strumLineNotes', i, 'angle', currentAngle)            
+                            noteTweenDirection('noteSustainDir' .. i, i, sustainDirection, 0.3)         
+                     end                                                                                
+              end
        end
-                                                                                                  --[[all this to control the note angles]]--
-              if curBeat % 2 == 0 then                                                            --|
-                     currentAngle = currentAngle + (5 * rotationDirection) -- Update angle        --|
-                     if currentAngle<0 then                                                       --|
-                            if curStep%2 == 0 then                                                --|
-                                   sustainDirection = -270 - (currentAngle/5)                     --|
-                            else                                                                  --|
-                                   sustainDirection = -270 + (currentAngle/5)                     --|
-                            end                                                                   --|
-                     elseif currentAngle > 0 then                                                 --|
-                            if curStep%2 == 0 then                                                --|
-                                   sustainDirection= 90 - (currentAngle/5)                        --|
-                            else                                                                  --|
-                                   sustainDirection = 90 + (currentAngle/5)                       --|
-                            end                                                                   --|
-                     else                                                                         --|
-                            sustainDirection = 90                                                 --|
-                     end                                                                          --|
-                     -- Reverse direction if angle hits 45 or -45                                 --|
-                     if currentAngle >= 45 or currentAngle <= -45 then                            --|
-                     rotationDirection = rotationDirection * -1                                   --|
-                     end                                                                          --|
-              -- Apply rotation to player strums (indexes 4 to 7)                                 --|
-              for i = 4, 7 do                                                                     --|
-                     setPropertyFromGroup('strumLineNotes', i, 'angle', currentAngle)             --|
-                     noteTweenDirection('noteSustainDir' .. i, i, sustainDirection, 0.3)          --|
-              end                                                                                 --|
-       end                                                                                        --|
+       if extremeNotesRotation == true then
+              if curBeat % 1 == 0 then
+                     currentAngle = currentAngle + (5 * rotationDirection) -- Update angle       
+                     if currentAngle<0 then                                                      
+                            if curStep%1 == 0 then                                               
+                                   sustainDirection = -270 - (currentAngle/5)                    
+                            else                                                                 
+                                   sustainDirection = -270 + (currentAngle/5)                    
+                            end                                                                  
+                     elseif currentAngle > 0 then                                                 
+                            if curStep%1 == 0 then                                                
+                                   sustainDirection= 90 - (currentAngle/5)                       
+                            else                                                                 
+                                   sustainDirection = 90 + (currentAngle/5)                      
+                            end                                                                  
+                     else                                                                        
+                            sustainDirection = 90                                                
+                     end                                                                         
+                     -- Reverse direction if angle hits 45 or -45                                
+                     if currentAngle >= 45 or currentAngle <= -45 then                           
+                     rotationDirection = rotationDirection * -1                                  
+                     end                                                                         
+                     -- Apply rotation to player strums (indexes 4 to 7)                                
+                     for i = 4, 7 do                                                                    
+                            setPropertyFromGroup('strumLineNotes', i, 'angle', currentAngle)            
+                            noteTweenDirection('noteSustainDir' .. i, i, sustainDirection, 0.3)         
+                     end                                                                                
+              end
+       end
 end
 function onStepHit()-- checks if the current step is equal to certain numbers
-       -- old defunct code i am too lazy to remove
-       -- if curStep == 1 then
-       --    applicationNameShift = 1
-       -- end
+       --[[
+        old defunct code i am too lazy to remove
+        if curStep == 1 then
+           applicationNameShift = 1
+        end
+       --]]
        if curStep == 278 then -- checks if the current step is equal to 278 
               startNoteTweening = true -- starts the code that moves the player notes around
               startTheHardPart = false -- disables the more difficult note movement
+              extremeNotesRotation = false
        end
        if curStep == 1808 then -- checks if the current step is equal to 1808    
               startTheHardPart = true -- starts the more difficult note movement
               startNoteTweening = false -- ends the more relaxed note movement
+              extremeNotesRotation = true
        end
 end
 function OnCreate()
@@ -96,39 +137,56 @@ function OnCreate()
               setPropertyFromClass("ClientPrefs", "downScroll", true) -- enables downscroll
               wasDownScrollOff = true -- saves the client prefrence for downscroll
        end
-       -- old defunct code i am too lazy to remove
-       -- runTimer("updateAppName", 1)
+       --[[
+        old defunct code i am too lazy to remove
+       runTimer("updateAppName", 1)
+       --]]
 end
 function onSongStart() -- code that executes when the song begins
-       -- old defunct code i am too lazy to remove
-       -- debugPrint("Application title change sucessful!")
+       --[[
+       old defunct code i am too lazy to remove
+       debugPrint("Application title change sucessful!")
+       --]]
        setPropertyFromClass("openfl.Lib", "application.window.x", 300)
        setPropertyFromClass("openfl.Lib", "application.window.y", 200)
-       -- old defunct code i am too lazy to remove
-      -- debugPrint("Application window sucessfully moved!")
+       --[[ 
+       old defunct code i am too lazy to remove
+       debugPrint("Application window sucessfully moved!")
+      --]]
        setPropertyFromGroup("playerStrums", 4, "x", defaultPlayerStrumX0 - 320)
        setPropertyFromGroup("playerStrums", 5, "x", defaultPlayerStrumX1 - 320)
        setPropertyFromGroup("playerStrums", 6, "x", defaultPlayerStrumX2 - 320)
        setPropertyFromGroup("playerStrums", 7, "x", defaultPlayerStrumX3 - 320)
        setProperty('healthGain', 1.01)
-       -- old defunct code i am too lazy to remove
-       -- originSS = getProperty('songspeed')
-       -- startingSS = originSS + 0.7
-       -- setProperty('songSpeed',startingSS)
+       --[[
+       old defunct code i am too lazy to remove
+        originSS = getProperty('songspeed')
+        startingSS = originSS + 0.7
+        setProperty('songSpeed',startingSS)
+        --]]
        setPropertyFromGroup("opponentStrums", 2, "x", defaultOpponentStrumX2 + 320)
        setPropertyFromGroup("opponentStrums", 3, "x", defaultOpponentStrumX3 + 320)
        setPropertyFromClass("openfl.Lib", "application.window.title", windowNameCycle)
 end
 function onUpdate(elapsed) -- code that executes every frame
-       if curStep > 1808 then --  checks if the current step in the song (tracked by curStep) is past step 1808.
-                              -- If this condition is true, the code inside runs
+       if curStep > 1808 then 
+              --[[^
+              checks if the current step in the song (tracked by curStep) is past step 1808.
+              If this condition is true, the code inside runs
+              --]]
               if delay == 0 then -- This checks if the variable delay is 0
-                     windowNameCycle = string.sub(windowNameCycle, -1) .. string.sub(windowNameCycle, 1, -2) -- string.sub(windowNameCycle, -1) gets the last character of the string.
-                                                                                                             -- string.sub(windowNameCycle, 1, -2) gets the string from the first character up to the second-to-last character.
+                     windowNameCycle = string.sub(windowNameCycle, -1) .. string.sub(windowNameCycle, 1, -2) 
+                     --[[ ^
+                     string.sub(windowNameCycle, -1) gets the last character of the string.
+                     string.sub(windowNameCycle, 1, -2) gets the string from the first character up to the second-to-last character.
+                     --]]
                      setWindowTitle(windowNameCycle) -- This sets the window title of the game to the new value of windowNameCycle, which is now rotated.                     
               end
-              delay = (delay + 1) % 3 -- This increments the delay variable by 1, and then uses modulo 3 to wrap it back around to 0 after it hits 2.
-                                      -- This creates a repeating cycle: 0 → 1 → 2 → 0 → ...
+              delay = (delay + 1) % 3 
+              --[[^
+               This increments the delay variable by 1, and then uses modulo 3 to wrap it back around to 0 after it hits 2.
+              This creates a repeating cycle: 0 → 1 → 2 → 0 → ...
+              --]]
        end
        Decider = math.random(1, 100)  -- Random value from 1 to 100 to decide behavior this frame              
        songPos = getSongPosition() -- Get current song position in milliseconds
@@ -213,22 +271,26 @@ function onUpdate(elapsed) -- code that executes every frame
               noteTweenX("defaultPlayerStrumX3", 7, defaultPlayerStrumX3 - 320 - randomPlayerstrumX4 * math.sin((currentBeat + 1.75) * math.pi), 0.5)
               -- End of hard part tweens
        end
-       -- old defunct code i am too lazy to remove
-       -- getMisses()
-       -- if misses == 1 then
-       --    for i in misses do
-       --        newScrollSpeed = originSS - 0.0
-       --        setProperty('songSpeed', newScrollSpeed)
-       --    end
-       -- else
-       --    newerScrollSpeed = newScrollSpeed - 0.02
-       --    newScrollSpeed = newerScrollSpeed
-       --    setProperty('songSpeed', newerScrollSpeed)
-       -- end
+       --[[ 
+       old defunct code i am too lazy to remove
+        getMisses()
+        if misses == 1 then
+           for i in misses do
+               newScrollSpeed = originSS - 0.0
+               setProperty('songSpeed', newScrollSpeed)
+           end
+        else
+           newerScrollSpeed = newScrollSpeed - 0.02
+           newScrollSpeed = newerScrollSpeed
+           setProperty('songSpeed', newerScrollSpeed)
+        end
+        --]]
 end
 function onDestroy() -- code that is executed when the game is closed or player exits the song through the pause menu
-       -- old defunct code i am too lazy to remove
-       -- debugPrint("Application window sucessfully moved!")
+       --[[ 
+       old defunct code i am too lazy to remove
+       debugPrint("Application window sucessfully moved!")
+       --]]
        if wasMidscrollOn == true then
               setPropertyFromClass("ClientPrefs", "middleScroll", true)
        end
--- PsychEngine/mods/pebbles/data/halcyonmodcharted/scrollspeedfckery.lua
+++ PsychEngine/mods/pebbles/data/halcyonmodcharted/scrollspeedfckery.lua
@@ -1,13 +1,109 @@
-function onBeatHit()
-    getMisses()
-    if misses==1 then 
-        for i in misses do
-            newScrollSpeed=originSS-0.02
-            setProperty('songSpeed',newScrollSpeed)
-        end
-    else
-        newerScrollSpeed=newScrollSpeed-0.02
-        newScrollSpeed=newerScrollSpeed
-        setProperty('songSpeed',newerScrollSpeed)
+local originSS = 0
+local startingSS = 0
+local minScrollSpeed = 0.1
+local decreasePerMiss = 0.02
+local currentSpeed = 0
+local lerpSpeed = 0.1
+local timeSinceLastMiss = 0
+local missCheckDelay = 2
+local recoveryFactor = 0.8
+local lastMisses = 0
+
+local baseDrainRate = 2 -- Base score drain per second
+local drainTimer = 0
+
+function onCreate()
+    originSS = getProperty('songSpeed')
+    startingSS = originSS + 0.09
+    currentSpeed = startingSS
+    setProperty('songSpeed', startingSS)
+end
+
+function onUpdate(elapsed)
+    local misses = getProperty('songMisses')
+    local targetSpeed = startingSS - (misses * decreasePerMiss)
+
+    if targetSpeed < minScrollSpeed then
+        targetSpeed = minScrollSpeed
     end
+
+    if misses > lastMisses then
+        timeSinceLastMiss = 0
+    else
+        timeSinceLastMiss = timeSinceLastMiss + elapsed
+    end
+
+    lastMisses = misses
+
+    if timeSinceLastMiss >= missCheckDelay then
+        local partialRecoveryTarget = currentSpeed + (startingSS - currentSpeed) * recoveryFactor
+        targetSpeed = partialRecoveryTarget
+    end
+
+    currentSpeed = lerp(currentSpeed, targetSpeed, lerpSpeed)
+    setProperty('songSpeed', currentSpeed)
+
+    -- --- SCORE DRAIN SECTION ---
+    drainTimer = drainTimer + elapsed
+    if drainTimer >= 0.05 then
+        local currentScore = getProperty('songScore')
+        local newScore = currentScore - (getComboAdjustedDrain() * drainTimer)
+        if newScore < 0 then newScore = 0 end
+        setProperty('songScore', math.floor(newScore))
+        drainTimer = 0
+    end
+end
+
+function onMiss()
+    timeSinceLastMiss = 0
+end
+
+function onNoteHit(id, direction, noteType, isSustainNote)
+    if isSustainNote then return end
+
+    local rating = getPropertyFromGroup('notes', id, 'rating')
+    local scoreBonus = getMissScoreBonus()
+    local pointsToAdd = 0
+
+    if rating == 'sick' then
+        pointsToAdd = 35000
+    elseif rating == 'good' then
+        pointsToAdd = 20000
+    elseif rating == 'bad' then
+        pointsToAdd = 10000
+    elseif rating == 'shit' then
+        pointsToAdd = 5000
+    end
+
+    pointsToAdd = math.floor(pointsToAdd * scoreBonus)
+    addScore(pointsToAdd)
+end
+
+function getMissScoreBonus()
+    local misses = getProperty('songMisses')
+    local bonus = 1 + (misses * 0.02)
+    if bonus > 2 then
+        bonus = 2
+    end
+    return bonus
+end
+
+function getComboAdjustedDrain()
+    local combo = getProperty('combo')
+    -- The bigger your combo, the less you drain
+    local drain = baseDrainRate
+
+    if combo >= 50 then
+        drain = baseDrainRate * 0.5 -- 50% slower drain
+    elseif combo >= 100 then
+        drain = baseDrainRate * 0.3 -- 70% slower drain
+    elseif combo >= 200 then
+        drain = baseDrainRate * 0.1 -- 90% slower drain (almost no drain)
+    end
+
+    return drain
+end
+
+function lerp(a, b, ratio)
+    return a + (b - a) * ratio
 end
