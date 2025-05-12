2eefb6baa716d0de4f72fa546e9119052b3cbc3c
NAKIII <graystripe.yaryan@gmail.com>
2025-05-12 13:55:06 -0500

    ...
    ...

--- PsychEngine/mods/Battle For Corrupted Island - Psych Port/scripts/Ending-Seasons/modcharts/Modchart.lua
+++ PsychEngine/mods/Battle For Corrupted Island - Psych Port/scripts/Ending-Seasons/modcharts/Modchart.lua
@@ -18,8 +18,6 @@ function onCreate()
     end
 end
 local startPassiveHealthDrain = false
-local originSS = 1
-local newScrollSpeed = 1
 local delay = 0
 local windowNameCycle = "Naki's FNF Modcharts - Ending Seasons - Composed by ZayDash Animates - Modchart and Rechart by just-Naki-here "
 
@@ -27,10 +25,6 @@ function onSongStart()
     for i = 0, 3 do
         setPropertyFromGroup('opponentStrums', i, 'alpha', 0.3)
     end
-
-    originSS = getProperty('songSpeed')
-    startingSS = originSS + 0.75
-    setProperty('songSpeed', startingSS)
 end
 
 function onBeatHit()
@@ -52,11 +46,6 @@ function onUpdatePost(elapsed)
             setWindowTitle(windowNameCycle)
         end
         delay = (delay + 1) % 3
-    end
-    -- Scroll Speed on Miss
-    if misses > 1 then
-        newScrollSpeed = math.max(0.05, newScrollSpeed - 0.02)
-        setProperty('songSpeed', newScrollSpeed)
     end
 end
 
--- PsychEngine/mods/Battle For Corrupted Island - Psych Port/scripts/Ending-Seasons/modcharts/windowandnoterotate.lua
+++ PsychEngine/mods/Battle For Corrupted Island - Psych Port/scripts/Ending-Seasons/modcharts/windowandnoterotate.lua
@@ -22,6 +22,10 @@ time = 0
 startNoteTweening = true
 startTheHardPart = false
 
+-- Assuming inChartEditor is defined elsewhere
+-- If not, define it here for testing purposes:
+inChartEditor = false  -- Change this based on your needs
+
 function onCreatePost()
     for i = 0, 7 do
         defaultStrumPos[i] = {
@@ -32,18 +36,18 @@ function onCreatePost()
 end
 
 function onUpdate(elapsed)
-    bpm = getProperty('curBpm')
+    local bpm = getProperty('curBpm')
     time = time + (elapsed * (bpm / 60))  -- Convert to beats
 
     -- If tweening is enabled, animate strums
     if startNoteTweening then
         -- Adjust for hard part
-        ampMult = startTheHardPart and 2 or 1
-        waveMult = startTheHardPart and 1.25 or 1
+        local ampMult = startTheHardPart and 2 or 1
+        local waveMult = startTheHardPart and 1.25 or 1
 
         for i = 0, 7 do
-            waveX = math.cos(time * waveSpeed * waveMult + i) * waveAmplitude * ampMult
-            waveY = math.sin(time * waveSpeed * waveMult + i) * waveAmplitude * ampMult
+            local waveX = math.cos(time * waveSpeed * waveMult + i) * waveAmplitude * ampMult
+            local waveY = math.sin(time * waveSpeed * waveMult + i) * waveAmplitude * ampMult
             setPropertyFromGroup('strumLineNotes', i, 'x', defaultStrumPos[i].x + waveX)
             setPropertyFromGroup('strumLineNotes', i, 'y', defaultStrumPos[i].y + waveY)
         end
@@ -51,31 +55,21 @@ function onUpdate(elapsed)
 
     -- Window movement (hard part makes it more chaotic)
     if not inChartEditor then
-        displayWidth = getPropertyFromClass('openfl.Lib', 'application.window.display.bounds.width')
-        displayHeight = getPropertyFromClass('openfl.Lib', 'application.window.display.bounds.height')
-        windowWidth = getPropertyFromClass('openfl.Lib', 'application.window.width')
-        windowHeight = getPropertyFromClass('openfl.Lib', 'application.window.height')
+        local displayWidth = getPropertyFromClass('openfl.Lib', 'application.window.display.bounds.width')
+        local displayHeight = getPropertyFromClass('openfl.Lib', 'application.window.display.bounds.height')
+        local windowWidth = getPropertyFromClass('openfl.Lib', 'application.window.width')
+        local windowHeight = getPropertyFromClass('openfl.Lib', 'application.window.height')
 
-        centerX = (displayWidth / 2) - (windowWidth / 2)
-        centerY = (displayHeight / 2) - (windowHeight / 2)
+        local centerX = (displayWidth / 2) - (windowWidth / 2)
+        local centerY = (displayHeight / 2) - (windowHeight / 2)
 
-        ampMult = startTheHardPart and 2 or 1
-        speedMult = startTheHardPart and 1.5 or 1
+        local ampMult = startTheHardPart and 2 or 1
+        local speedMult = startTheHardPart and 1.5 or 1
 
-        windowX = math.cos(time * windowSpeed * speedMult) * windowAmplitude * ampMult
-        windowY = math.sin(time * windowSpeed * speedMult) * windowAmplitude * ampMult
+        local windowX = math.cos(time * windowSpeed * speedMult) * windowAmplitude * ampMult
+        local windowY = math.sin(time * windowSpeed * speedMult) * windowAmplitude * ampMult
 
         setPropertyFromClass('openfl.Lib', 'application.window.x', centerX + windowX)
         setPropertyFromClass('openfl.Lib', 'application.window.y', centerY + windowY)
-    end
-end
-
-function onStepHit()
-    if curStep == 2980 then
-        startNoteTweening = true
-    end
-    if curStep == 4768 then
-        startTheHardPart = true
-        startNoteTweening = false
     end
 end
